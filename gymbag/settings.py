ba6021558c3ad0951e981b5dc8cf14027bed2896
sherryrich <ricsherry@gmail.com>
2022-09-21 11:37:59 +0000

    Added Djanog messaging framework working with toats

--- gymbag/settings.py
+++ gymbag/settings.py
@@ -79,6 +79,7 @@ TEMPLATES = [
     },
 ]
 
+MESSAGE_STORAGE = 'django.contrib.messages.storage.session.SessionStorage'
 
 AUTHENTICATION_BACKENDS = [
     # Needed to login by username in Django admin, regardless of `allauth`
