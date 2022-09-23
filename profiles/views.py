dcc7ed07fc5139f609d5cfea51314adbd9eaa95e
sherryrich <ricsherry@gmail.com>
2022-09-23 14:17:18 +0000

    added @login_required to secure product and profile views

--- profiles/views.py
+++ profiles/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 
 from .models import UserProfile
 from .forms import UserProfileForm
@@ -8,6 +9,7 @@ from checkout.models import Order
 
 # Create your views here.
 
+@login_required
 def profile(request):
     """ Display the user's profile. """
     profile = get_object_or_404(UserProfile, user=request.user)
