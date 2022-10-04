8b9e4d99808d4ebf8671d1f4ec72f960ea71dd48
sherryrich <ricsherry@gmail.com>
2022-10-04 15:36:00 +0000

    Removed secret key

--- gymbag/settings.py
+++ gymbag/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-+g=!a1@c-h3)xz5g3=$oqvhfim+ztfd8o-+5zpl*+jzs&7+&$v')
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = development
