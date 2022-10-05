0e1431c795731cfafdc835723423a6ba23c14228
sherryrich <ricsherry@gmail.com>
2022-10-05 07:44:21 +0000

    testing deployment

--- gymbag/settings.py
+++ gymbag/settings.py
@@ -140,12 +140,13 @@ WSGI_APPLICATION = 'gymbag.wsgi.application'
 #         }
 #     }
 
+
 DATABASES = {
-         'default': {
-             'ENGINE': 'django.db.backends.sqlite3',
-             'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-         }
-     }
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
