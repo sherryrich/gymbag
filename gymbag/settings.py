6566dd763ed913b86dcb9536f19090ca6de4f83f
sherryrich <ricsherry@gmail.com>
2022-10-05 10:32:35 +0000

    attempting aws deployment

--- gymbag/settings.py
+++ gymbag/settings.py
@@ -185,7 +185,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 if 'USE_AWS' in os.environ:
     # Bucket Config
-    AWS_STORAGE_BUCKET_NAME = 'sherryrich-gymbag-2'
+    AWS_STORAGE_BUCKET_NAME = 'sherryrich-gymbag2'
     AWS_S3_REGION_NAME = 'us-east-1'
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
