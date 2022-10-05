f298e5f692e982f3af5814c21febd8fb3af38086
sherryrich <ricsherry@gmail.com>
2022-10-05 08:33:46 +0000

    set Debug to development in os.environ

--- gymbag/settings.py
+++ gymbag/settings.py
@@ -27,10 +27,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = development
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['sherryrich-gymbag.herokuapp.com', 'localhost']
-
 
 # Application definition
 
