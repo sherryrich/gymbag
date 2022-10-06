6461d705855aaa270041612b3ec3beed649e0a5e
sherryrich <ricsherry@gmail.com>
2022-10-06 19:02:38 +0000

    Added 404 error page

--- gymbag/views.py
+++ gymbag/views.py
@@ -1,5 +1,11 @@
+from django.shortcuts import render
+
+
 class about(View):
     """View for displaying the 'About' page."""
     template_name = "about.html"
 
 
+def handler404(request, exception):
+    """ Error Handler 404 - Page Not Found """
+    return render(request, "errors/404.html", status=404)
