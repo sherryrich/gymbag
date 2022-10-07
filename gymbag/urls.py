9e3669f1d564e4fc4d1711519bc8260ec08a5ae6
sherryrich <ricsherry@gmail.com>
2022-10-07 11:49:32 +0000

    added 500 page

--- gymbag/urls.py
+++ gymbag/urls.py
@@ -20,7 +20,6 @@ from django.conf.urls.static import static
 from django.views.generic import TemplateView
 from contact.views import contact
 from installation.views import Installation
-from .views import handler404
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -37,4 +36,3 @@ urlpatterns = [
     path('installation/', Installation, name='installation'),
     path('news/', TemplateView.as_view(template_name="news.html"), name='news'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-handler404 = 'gymbag.views.handler404'
--- gymbag/views.py
+++ gymbag/views.py
@@ -1,14 +1,5 @@
 from django.views.generic import View
-from django.shortcuts import render
 
 class about(View):
     """View for displaying the 'About' page."""
     template_name = "about.html"
-
-def handler404(request, exception):
-    """ Error Handler 404 - Page Not Found """
-    return render(request, "errors/404.html", status=404)
-
-def handler500(request, exception):
-    """ Error Handler 500 - Page Not Found """
-    return render(request, "errors/500.html", status=500)
