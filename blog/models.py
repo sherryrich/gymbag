d2fe0ccb62d4e64552fae865a093cafdc25341f3
sherryrich <ricsherry@gmail.com>
2022-10-10 16:03:37 +0000

    Added commenting functionality to news.html

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.template.defaultfilters import slugify
 
 # models created below
 # creating ERD into model, cascade if profile
