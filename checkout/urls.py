e8f11ef4223f58a051245771059f54af5b1062d9
sherryrich <ricsherry@gmail.com>
2022-10-13 14:12:57 +0000

    removed order status search feature as this is shown on my account page now

--- checkout/urls.py
+++ checkout/urls.py
@@ -6,8 +6,5 @@ urlpatterns = [
     path('', views.checkout, name='checkout'),
     path('checkout_success/<order_number>', views.checkout_success, name='checkout_success'),
     path('cache_checkout_data/', views.cache_checkout_data, name='cache_checkout_data'),
-    path('orderstatus/', views.orderstatus, name='orderstatus'),
-    path('search_orders/', views.search_orders, name='search_orders'),
-    path('order_list/', views.search_order, name='order_list'),
     path('wh/', webhook, name='webhook'),
 ]
