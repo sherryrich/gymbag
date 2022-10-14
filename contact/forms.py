a518d1aad06d9ff2d1257a3e6ca4f026298d7b0b
sherryrich <ricsherry@gmail.com>
2022-10-14 11:16:12 +0000

    updated styling on installation and contact forms

--- contact/forms.py
+++ contact/forms.py
@@ -7,3 +7,9 @@ class ContactForm(forms.ModelForm):
     class Meta:
         model = Contact
         fields = "__all__"
+        widgets = {
+            'name': forms.TextInput(attrs={'class': 'form-control'}),
+            'email': forms.TextInput(attrs={'class': 'form-control'}),
+            'body': forms.TextInput(attrs={'class': 'form-control'}),
+            'contact_reason': forms.Select(attrs={'class': 'form-control'}),
+        }
--- installation/forms.py
+++ installation/forms.py
@@ -12,9 +12,17 @@ class InstallationForm(forms.ModelForm):
             'last_name',
             'phone_number',
             'email_address',
-            'date',
             'installation_type',
+            'date',
         )
+        widgets = {
+            'first_name': forms.TextInput(attrs={'class': 'form-control'}),
+            'last_name': forms.TextInput(attrs={'class': 'form-control'}),
+            'phone_number': forms.NumberInput(attrs={'class': 'form-control'}),
+            'email_address': forms.EmailInput(attrs={'class': 'form-control'}),
+            'installation_type': forms.Select(attrs={'class': 'form-control'}),
+            'date': forms.SelectDateWidget(attrs={'class': 'form-control'}),
+        }
 
     
 
