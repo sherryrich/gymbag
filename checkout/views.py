0614671d5960d1fe660f47b437199ae2d32f6649
sherryrich <ricsherry@gmail.com>
2022-10-28 09:22:23 +0000

    Refactor code to meet pep8 standard

--- checkout/views.py
+++ checkout/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404, HttpResponse  # noqa
-from django.http import Http404
 from django.views.decorators.http import require_POST
 from django.contrib import messages
 from django.conf import settings
--- contact/views.py
+++ contact/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404, redirect
+from django.shortcuts import render
 from .forms import ContactForm
 from django.http import HttpResponseRedirect
 from django.contrib import messages
