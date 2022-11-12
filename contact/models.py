7ca82557625c1cfdd29a17b889b4dfd4d089dd61
sherryrich <ricsherry@gmail.com>
2022-11-12 09:39:39 +0000

    Update contact - models.py

--- contact/models.py
+++ contact/models.py
@@ -1,5 +1,4 @@
 from django.db import models
-# Create your models here.
 
 
 class Contact(models.Model):
@@ -11,14 +10,18 @@ class Contact(models.Model):
         ('feedback', 'Feedback'),
         ('other', 'Other'),
     ]
-    name = models.CharField(max_length=80)
+    name = models.CharField(
+        max_length=80
+        )
     email = models.EmailField()
     body = models.TextField()
-    created_on = models.DateTimeField(auto_now_add=True)
+    created_on = models.DateTimeField(
+        auto_now_add=True
+        )
     contact_reason = models.CharField(
         max_length=24,
         choices=REASON_FOR_CONTACT,
-        default='please select below')
-
+        default='please select below'
+        )
     def __str__(self):
         return self.name
